0fd401687fef7bdbe42985a8206ad950de40a89d
Noah Rettig <noah.rettig.s@gmail.com>
2025-03-18 12:16:47 -0500

    Sidebar expansion changes

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -7,45 +7,47 @@ export default function Sidebar() {
     const [isFolder2Open, setIsFolder2Open] = useState(false);
 
     return (
-        <aside className="w-64 h-screen bg-gray-100 p-4">
+        <aside className="w-64 bg-gray-100 p-4 overflow-y-auto h-[calc(100vh-4rem)]">
             <h2 className="text-2xl font-bold mb-4">Sidebar</h2>
             <ul>
-                <li>
+                <li className="flex justify-between items-center">
+                    <span className="p-2">Folder 1</span>
                     <button
                         onClick={() => setIsFolder1Open(!isFolder1Open)}
-                        className="w-full text-left p-2 hover:bg-gray-200"
+                        className="p-2 hover:bg-gray-200"
                     >
-                        Folder 1
+                        {isFolder1Open ? '-' : '+'}
                     </button>
-                    {isFolder1Open && (
-                        <ul className="pl-4">
-                            <li>
-                                <Link href="#" className="block p-2 hover:bg-gray-200">file1.mp4</Link>
-                            </li>
-                            <li>
-                                <Link href="#" className="block p-2 hover:bg-gray-200">file2.mp4</Link>
-                            </li>
-                        </ul>
-                    )}
                 </li>
-                <li>
+                {isFolder1Open && (
+                    <ul className="pl-4">
+                        <li>
+                            <Link href="#" className="block p-2 hover:bg-gray-200">file1.mp4</Link>
+                        </li>
+                        <li>
+                            <Link href="#" className="block p-2 hover:bg-gray-200">file2.mp4</Link>
+                        </li>
+                    </ul>
+                )}
+                <li className="flex justify-between items-center">
+                    <span className="p-2">Folder 2</span>
                     <button
                         onClick={() => setIsFolder2Open(!isFolder2Open)}
-                        className="w-full text-left p-2 hover:bg-gray-200"
+                        className="p-2 hover:bg-gray-200"
                     >
-                        Folder 2
+                        {isFolder2Open ? '-' : '+'}
                     </button>
-                    {isFolder2Open && (
-                        <ul className="pl-4">
-                            <li>
-                                <Link href="#" className="block p-2 hover:bg-gray-200">file3.mp4</Link>
-                            </li>
-                            <li>
-                                <Link href="#" className="block p-2 hover:bg-gray-200">file4.mp4</Link>
-                            </li>
-                        </ul>
-                    )}
                 </li>
+                {isFolder2Open && (
+                    <ul className="pl-4">
+                        <li>
+                            <Link href="#" className="block p-2 hover:bg-gray-200">file3.mp4</Link>
+                        </li>
+                        <li>
+                            <Link href="#" className="block p-2 hover:bg-gray-200">file4.mp4</Link>
+                        </li>
+                    </ul>
+                )}
             </ul>
         </aside>
     );
